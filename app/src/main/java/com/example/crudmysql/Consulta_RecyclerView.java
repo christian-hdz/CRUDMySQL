3415534cc75447614c0ddf98982db9774634047a
christian-hdz <75574579hdz@gmail.com>
2019-11-20 23:10:16 -0600

    se agrega metodo cargar productos

--- app/src/main/java/com/example/crudmysql/Consulta_RecyclerView.java
+++ app/src/main/java/com/example/crudmysql/Consulta_RecyclerView.java
@@ -2,12 +2,25 @@ package com.example.crudmysql;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Consulta_RecyclerView extends AppCompatActivity {
@@ -45,5 +58,70 @@ public class Consulta_RecyclerView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta__recycler_view);
+        ///y esto para pantalla completa (oculta incluso la barra de estado)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        productosList = new ArrayList<>();
+
+
+
+        //Toast.makeText(this, "Si", Toast.LENGTH_SHORT).show();
+
+        loadProductos();
+
+    }
+
+
+    private void loadProductos() {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        //Toast.makeText(Consulta_RecyclerView.this, ""+response, Toast.LENGTH_SHORT).show();
+
+                        try {
+                            JSONArray array = new JSONArray(response);
+                            int totalEncontrados = array.length();
+                            Toast.makeText(Consulta_RecyclerView.this, "Total: "+totalEncontrados, Toast.LENGTH_SHORT).show();
+
+                            for (int i = 0; i < array.length(); i++) {
+
+                                JSONObject articulosObject = array.getJSONObject(i);
+
+                                //String img = articulosObject.getString("imagen");
+                                //Toast.makeText(Consulta_RecyclerView.this, ""+img, Toast.LENGTH_SHORT).show();
+
+                                /*int codigo = articulosObject.getInt("codigo");
+                                String descripcion = articulosObject.getString("descripcion");
+                                double precio = articulosObject.getDouble("precio");
+                                String img = articulosObject.getString("imagen");
+                                Productos objeto = new Productos(codigo, descripcion, precio, img);
+                                productosList.add(objeto);*/
+
+                                productosList.add(new Productos(
+                                        articulosObject.getInt("codigo"),
+                                        articulosObject.getString("descripcion"),
+                                        articulosObject.getDouble("precio"),
+                                        articulosObject.getString("imagen")
+                                ));
+                            }
+
+                            adapter = new ProductsAdapter(Consulta_RecyclerView.this, productosList);
+                            recyclerView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Consulta_RecyclerView.this, "Error. Compruebe su acceso a Internet.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
