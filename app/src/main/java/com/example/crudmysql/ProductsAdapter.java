836dc38f130e541bb1eede5aeb7cfb9ff132b0a7
christian-hdz <75574579hdz@gmail.com>
2019-11-20 22:49:32 -0600

    Productadapter y medio codigo

---
+++ app/src/main/java/com/example/crudmysql/ProductsAdapter.java
@@ -0,0 +1,68 @@
+package com.example.crudmysql;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+//import android.support.v7.widget.RecyclerView;
+
+
+public class ProductsAdapter extends RecyclerView.Adapter<ProductsAdapter.ProductViewHolder> {
+
+
+    private Context mCtx;
+    private List<Productos> productList;
+
+    public ProductsAdapter(Context mCtx, List<Productos> productList) {
+        this.mCtx = mCtx;
+        this.productList = productList;
+    }
+
+    @Override
+    public ProductViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.list_layout, null);
+        return new ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ProductViewHolder holder, int position) {
+        Productos product = productList.get(position);
+
+        //loading the image
+
+        String im = product.getImagen();
+        //Toast.makeText(mCtx, ""+im, Toast.LENGTH_SHORT).show();
+
+
+        if(im.isEmpty()) {
+            holder.imageView.setImageResource(R.drawable.imgnoencontrada);
+            holder.textViewCodigo1.setText(String.valueOf(product.getCodigo()));
+            holder.textViewDescripcion1.setText(product.getDescripcion());
+            holder.textViewPrecio1.setText(String.valueOf(product.getPrecio()));
+
+        }else{
+            Glide.with(mCtx)
+                    .load(product.getImagen())
+                    .into(holder.imageView);
+
+            holder.textViewCodigo1.setText(String.valueOf(product.getCodigo()));
+            holder.textViewDescripcion1.setText(product.getDescripcion());
+            holder.textViewPrecio1.setText(String.valueOf(product.getPrecio()));
+        }
+
+    }
+
+
+
+
+}
